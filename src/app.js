df30108bcc54192b3967a608fe7e0329fad08d23
KateLun <ekat.goryacheva@mail.ru>
2022-12-18 01:18:47 +0300

    запросы на сервер

---
+++ src/app.js
@@ -0,0 +1,57 @@
+async function makeTours() {
+    const responce = await fetch(
+        'https://www.bit-by-bit.ru/api/student-projects/tours'
+    )
+    const tours = await responce.json()
+
+    console.log(tours)
+
+    const container = document.getElementById('container')
+    tours.forEach((tour) => {
+    container.innerHTML += `
+        <div class="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col justify-between">
+            <img class="h-72 w-full" src="${tour.image}" />
+
+            <div class="p-6">
+                <div>
+                    <p class="text-green-800 font-semibold mt-3 text-xl">${tour.country}</p>
+                    <p class="text-grey-500 mt-3">${tour.city}</p>
+
+                    <div class="mt-3 text-grey-500 text-sm flex items-start">
+                        <p class="text-grey-600 font-medium hover:underline">
+                             <a href="#">${tour.hotelName}</a>
+                        </p>
+                        <span class="mx-2" aria-hidden="true">&ndash;</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                        </svg>
+                        <span class="ml-1">${tour.rating}</span>
+                    </div>
+
+                    <div class="flex-blockmini text-grey-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                        </svg>
+                        <span class="ml-1">${tour.price}</span>
+                    </div>
+
+                    <div class="flex-blockmini text-grey-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                        </svg>
+                        <span class="ml-1">${tour.startTime}-${tour.endTime}</span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="flex flex-col justify-center px-6 pb-6">
+                <button class="btn-primary mt-4">Подробнее</button>
+                <button class="btn-primary mt-4">Добавить в избранное</button>
+            </div>
+
+        </div>
+                
+    `})
+}
+
+makeTours()
